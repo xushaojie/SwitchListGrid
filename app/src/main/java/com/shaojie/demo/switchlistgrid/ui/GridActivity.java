5002ee34b3921bd314dd3fcb26f28eb85267f167
xushaojie <shaojie.xu@husor.com>
2016-05-24 10:47:04 +0800

    add GridActivity

--- app/src/main/java/com/shaojie/demo/switchlistgrid/ui/GridActivity.java
+++ app/src/main/java/com/shaojie/demo/switchlistgrid/ui/GridActivity.java
@@ -1,28 +1,32 @@
-package com.shaojie.demo.switchlistgrid;
+package com.shaojie.demo.switchlistgrid.ui;
 
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
 import android.support.annotation.DrawableRes;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.shaojie.demo.switchlistgrid.AutoFitRecyclerView;
+import com.shaojie.demo.switchlistgrid.Item;
+import com.shaojie.demo.switchlistgrid.R;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 感谢 https://github.com/chiuki/android-recyclerview
+ *
  */
-public class MainActivity extends AppCompatActivity {
+public class GridActivity extends AppCompatActivity {
 
     @ColorInt
     private static final int[] BG_COLORS = {0xfff25f8c, 0xfffb7f77, 0xfffcc02c, 0xff2fcc87,
@@ -32,25 +36,27 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.card_cover_d, R.drawable.card_cover_e, R.drawable.card_cover_f};
 
     private AutoFitRecyclerView mRecyclerView;
-    private MyAdapter mAdapter;
     private List<Item> mItems;
-    private int mMode;
-
     private LayoutInflater mLayoutInflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_grid);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("GridActivity");
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         mItems = initData();
-        mMode = getMode();
 
         mLayoutInflater = LayoutInflater.from(this);
         mRecyclerView = (AutoFitRecyclerView) findViewById(R.id.recycler_view);
         mRecyclerView.setHasFixedSize(true);
-        mAdapter = new MyAdapter();
-        mRecyclerView.setAdapter(mAdapter);
+        GridAdapter adapter = new GridAdapter();
+        mRecyclerView.setAdapter(adapter);
     }
 
     private List<Item> initData() {
@@ -61,22 +67,6 @@ public class MainActivity extends AppCompatActivity {
             records.add(record);
         }
         return records;
-    }
-
-    private int getMode() {
-        return SharedPreferUtil.get("sp_key_switch_mode", AutoFitRecyclerView.MODE_LIST);
-    }
-
-    private void updateMode() {
-        SharedPreferUtil.put("sp_key_switch_mode", mMode);
-    }
-
-    private void toggleDisplayMode() {
-        mMode = mRecyclerView.toggleMode();
-        mAdapter = new MyAdapter();
-        mRecyclerView.setAdapter(mAdapter);
-        mRecyclerView.scheduleLayoutAnimation();
-        updateMode();
     }
 
     private View.OnClickListener mOnClickListener = new View.OnClickListener() {
@@ -101,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
         return BG_COVERS[index];
     }
 
-    private class MyAdapter extends RecyclerView.Adapter {
+    private class GridAdapter extends RecyclerView.Adapter {
 
         private static final int TYPE_HEADER = 0;
         private static final int TYPE_ITEM = 1;
@@ -109,15 +99,13 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onAttachedToRecyclerView(RecyclerView recyclerView) {
             super.onAttachedToRecyclerView(recyclerView);
-            if (mMode == AutoFitRecyclerView.MODE_GRID) {
-                final GridLayoutManager manager = (GridLayoutManager) recyclerView.getLayoutManager();
-                manager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
-                    @Override
-                    public int getSpanSize(int position) {
-                        return getItemViewType(position) == TYPE_HEADER ? manager.getSpanCount() : 1;
-                    }
-                });
-            }
+            final GridLayoutManager manager = (GridLayoutManager) recyclerView.getLayoutManager();
+            manager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+                @Override
+                public int getSpanSize(int position) {
+                    return getItemViewType(position) == TYPE_HEADER ? manager.getSpanCount() : 1;
+                }
+            });
         }
 
         @Override
@@ -136,16 +124,9 @@ public class MainActivity extends AppCompatActivity {
                 v = mLayoutInflater.inflate(R.layout.recycleview_header, parent, false);
                 return new HeaderHolder(v);
             } else {
-                switch (mMode) {
-                    case AutoFitRecyclerView.MODE_LIST:
-                        v = mLayoutInflater.inflate(R.layout.list_item, parent, false);
-                        return new ListHolder(v);
-                    case AutoFitRecyclerView.MODE_GRID:
-                        v = mLayoutInflater.inflate(R.layout.grid_item, parent, false);
-                        return new GridHolder(v);
-                }
+                v = mLayoutInflater.inflate(R.layout.grid_item, parent, false);
+                return new GridHolder(v);
             }
-            return null;
         }
 
         @Override
@@ -156,20 +137,12 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 Item item = mItems.get(position - 1);// 减去header的位置
                 @ColorInt int colorRes = getBgColor(position);
-                if (holder instanceof ListHolder) {
-                    ListHolder listHolder = (ListHolder) holder;
-                    listHolder.nameText.setText(item.getName());
-                    listHolder.colorView.setBackgroundColor(colorRes);
-                    listHolder.cardView.setTag(item);
-                    listHolder.cardView.setOnClickListener(mOnClickListener);
-                } else if (holder instanceof GridHolder) {
-                    GridHolder gridHolder = (GridHolder) holder;
-                    gridHolder.nameText.setText(item.getName());
-                    gridHolder.colorView.setBackgroundColor(colorRes);
-                    gridHolder.coverView.setImageResource(getBgCover(position));
-                    gridHolder.cardView.setTag(item);
-                    gridHolder.cardView.setOnClickListener(mOnClickListener);
-                }
+                GridHolder gridHolder = (GridHolder) holder;
+                gridHolder.nameText.setText(item.getName());
+                gridHolder.colorView.setBackgroundColor(colorRes);
+                gridHolder.coverView.setImageResource(getBgCover(position));
+                gridHolder.cardView.setTag(item);
+                gridHolder.cardView.setOnClickListener(mOnClickListener);
             }
         }
 
@@ -192,22 +165,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        public class ListHolder extends RecyclerView.ViewHolder {
-
-            public ImageView avatarView;
-            public TextView nameText;
-            public View colorView;
-            public CardView cardView;
-
-            public ListHolder(View v) {
-                super(v);
-                cardView = (CardView) v.findViewById(R.id.list_item_card_view);
-                avatarView = (ImageView) v.findViewById(R.id.list_item_avatar);
-                nameText = (TextView) v.findViewById(R.id.list_item_name);
-                colorView = v.findViewById(R.id.list_item_color);
-            }
-        }
-
         public class GridHolder extends RecyclerView.ViewHolder {
 
             public ImageView coverView;
@@ -226,23 +183,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        menu.clear();
-        getMenuInflater().inflate(R.menu.main_switch_menu, menu);
-        if (mMode == AutoFitRecyclerView.MODE_LIST) {
-            menu.findItem(R.id.menu_item_switch_mode).setIcon(R.drawable.ic_action_switch_grid);
-        } else {
-            menu.findItem(R.id.menu_item_switch_mode).setIcon(R.drawable.ic_action_switch_list);
-        }
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.menu_item_switch_mode:
-                toggleDisplayMode();
-                invalidateOptionsMenu();
+            case android.R.id.home:
+                finish();
                 return true;
         }
         return super.onOptionsItemSelected(item);
